6f06ffd6b66bb6edfb6217a49829a02a4cdf3186
Jonathan Zacsh <jzacsh@gmail.com>
2010-09-14 20:32:09 -0400

    hw 0928 finished

--- hw/0928/tip.java
+++ hw/0928/tip.java
@@ -6,6 +6,10 @@ public class tip {
     Scanner input = new Scanner(System.in);
 
     System.out.printf("Please input a bill total:\n");
+    total = input.nextFloat();
+
+    tip   = (float)0.15 * total;
+    total = tip + total;
 
     System.out.printf("\nYour Tip is:   %s\nYour Total is: %s\n", tip, total);
 
