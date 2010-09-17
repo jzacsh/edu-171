b652e20d03db65a71eb44291fd5c88a95590693c
Jonathan Zacsh <jzacsh@gmail.com>
2010-09-17 01:08:41 -0400

    dont understand vague javac compiler error on looping version

---
+++ hw/0928/tipcmd.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class tip {
+  public static void main(String[] args) {
+    float total = 0, tipamt = 0;
+    for (i=0, i <= args.length, i++) {
+      if (args.length != 0) {
+        total = args(i+1);
+      }
+      else {
+        Scanner input = new java.util.Scanner(System.in);
+        System.out.printf("Please input a bill total: ");
+        total = input.nextFloat();
+      }
+
+      tipamt = (float)0.15 * total;
+      total  = tipamt + total;
+
+      System.out.printf("\nYour Tip is:   %s\nYour Total is: %s\n", tipamt, total);
+    }
+  }
+}
