b333e72a8f0db073a6ffdf162fb84edf6f6f07cf
Jonathan Zacsh <jzacsh@gmail.com>
2010-09-26 23:29:44 -0400

    hw 1207 - stopping, as i may totally rewrite my plan based on _proper_ readings of this chapter

---
+++ hw/1207/wkHours.java
@@ -0,0 +1,30 @@
+/**
+ * Class for managing a given week of employee hours.
+ */
+public class wkHours {
+  public static void main() {
+   // Pass an array of total hours, in its original employee-order to
+   // sortTotals() to receive a new array of each employees' newly sorted position
+  }
+
+  /**
+   * Re-sort an array of numbers, descending.
+   *
+   *  @param  array  Array of total hours for each employee.
+   *  @return array  New sort order for each employee's number.
+   *    Original numeric position of each employee, stored in new positions
+   *    throughout array, representing the sort order.
+   */
+  protected double[] sortTotals(double[] total) {
+    //
+  }
+
+  /**
+   * Total the values in a given array.
+   */
+  protected double wkTotal() {
+  }
+
+  protected double[] getTotals() {
+  }
+}
