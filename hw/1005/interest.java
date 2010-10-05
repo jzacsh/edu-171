8242ad23c47845f8d8acf0d59caf5a9c0204cfbf
Jonathan Zacsh <jzacsh@gmail.com>
2010-10-05 12:38:01 -0400

    out file for hw 1005; professor request for intro-stamping

--- hw/1005/interest.java
+++ hw/1005/interest.java
@@ -12,20 +12,22 @@ import javax.swing.JOptionPane;
 public class interest {
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
-    System.out.printf("I calculate Annual Interest, given an interest rate and balance.\n");
+    String intro = "\nJonathan Zacsh <jzacsh@gmail.com>\n";
+    intro += "assignment: pg 65; que. #2.13; due: 10/05\n";
+    System.out.printf("%s\nI calculate Annual Interest, given an interest rate and balance.\n", intro);
     float balance = 0, rate = 0, interest = 0;
 
-    System.out.printf("Please input a balance       : ");
+    System.out.printf("Please input a balance       : $");
     balance = stdin.nextFloat();
-    System.out.printf("Please input an interest rate: ");
+    System.out.printf("Please input an interest rate: %%");
     rate    = stdin.nextFloat();
 
     interest = balance * (rate / 1200);
     if (args.length != 0) {
-      JOptionPane.showMessageDialog(null, "\nYour Interest is: " + interest + "\n");
+      JOptionPane.showMessageDialog(null, "\nYour Interest is: $" + interest + "\n");
     }
     else {
-      System.out.printf("\nYour Interest is:%.2f \n", interest);
+      System.out.printf("\nYour Interest is: $%.2f \n", interest);
     }
   }
 }
