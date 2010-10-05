0898c5174ef29096dde663c7f6304a589e805032
Jonathan Zacsh <jzacsh@gmail.com>
2010-10-04 20:25:14 -0400

    hw 0928; per professor request: have program prompt for gratuity.

--- hw/0928/tip.java
+++ hw/0928/tip.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class tip {
   public static void main(String[] args) {
-    double total = 0, tip = 0;
+    double total = 0, tip = 0, grat = 0.15;
     Scanner input = new Scanner(System.in);
 
-    System.out.printf("Please input a bill total: $");
+    System.out.printf("Please input a bill total:            : $");
     total = input.nextDouble();
+    System.out.printf("Please input a gratuity: (eg.: '0.15'):  ");
+    grat = input.nextDouble();
 
-    tip   = 0.15 * total;
+    tip   = grat * total;
     total = tip + total;
 
     System.out.printf("\nYour Tip is:   $%.2f\nYour Total is: $%.2f\n", tip, total);
