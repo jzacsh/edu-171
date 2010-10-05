41dde8fbc44f9128956a13959cb60fb1f482e54d
Jonathan Zacsh <jzacsh@gmail.com>
2010-10-04 20:18:42 -0400

    hw0928; per professor request: use 2 decimal places per professor request. using doubles, per my own desire.

--- hw/0928/tip.java
+++ hw/0928/tip.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class tip {
   public static void main(String[] args) {
-    float total = 0, tip = 0;
+    double total = 0, tip = 0;
     Scanner input = new Scanner(System.in);
 
-    System.out.printf("Please input a bill total:\n");
-    total = input.nextFloat();
+    System.out.printf("Please input a bill total: $");
+    total = input.nextDouble();
 
-    tip   = (float)0.15 * total;
+    tip   = (double)0.15 * total;
     total = tip + total;
 
-    System.out.printf("\nYour Tip is:   %s\nYour Total is: %s\n", tip, total);
+    System.out.printf("\nYour Tip is:   $%.2f\nYour Total is: $%.2f\n", tip, total);
   }
 }
