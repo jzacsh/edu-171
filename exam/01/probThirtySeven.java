8a6effd509685448932881f0ae5c351f55cd7a0e
Jonathan Zacsh <jzacsh@gmail.com>
2010-10-19 19:59:48 -0400

    exam 01

---
+++ exam/01/probThirtySeven.java
@@ -0,0 +1,9 @@
+public class probThirtySeven {
+  public static void main (String args[]) {
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.println('1');
+      }
+    } //finished
+  }
+}
