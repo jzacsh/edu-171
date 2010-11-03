c01cc6e53591ff58e6c409171758241d665677f0
Jonathan Zacsh <jzacsh@gmail.com>
2010-11-02 20:32:09 -0400

    beginning of 1102 source

--- hw/1102/InvestmentValueLab05.java
+++ hw/1102/InvestmentValueLab05.java
@@ -27,3 +27,16 @@ future value for the years from 1 to 30, as shown below:
    29      13467.25
    30      14730.57
 */
+import java.util.Scanner;
+
+public class InvestmentValueLab05 {
+  public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+    // start professor requisite //////////////////////////////////////////////////////////////////
+    String intro = "\n\nJonathan Zacsh <jzacsh@gmail.com>; COMP-171-801RL\n";
+    intro += "\npg 163; que. #5.7; due: 11/02\n";
+    // end professor requisite ////////////////////////////////////////////////////////////////////
+    String summary = "I'm an investment calculator. All I need is the interest rate and duration (in years).\n";
+    System.out.printf("%s\n%s", intro, summary);
+  }
+}
