f3f9e2542d416aecbab79e6f3487054fed9b5d7f
Jonathan Zacsh <jzacsh@gmail.com>
2010-11-07 17:03:22 -0500

    hw 1214; #11; more file handling usage and most code finished

--- hw/1214/ReadFiles11.java
+++ hw/1214/ReadFiles11.java
@@ -24,28 +24,40 @@ public class ReadFiles11 {
     System.out.printf("%s\n%s\n", intro, summary);
 
     //set constants
+    final int  MAX = 300; //highest value for random number generation
     final int  QUANTITY = 100; //number of datas to be passed around
     final char DELIM = ' '; //delimeter to use for seperating datas
 
     //request user input of file name
     String filename;
     filename = stdin.nextString();
-    java.io.File file = new java.io.File("./" + filename + ".txt");
+    filename = "./" + filename + ".txt";
 
-    //ensure data is available
-    if (!file.isFile()) {
-      //file does not exist, create now.
-      AppendTxtFile(file);
-    }
+    //file objects
+    java.io.File fd = new java.io.File(filename); //create file descriptor
+    java.io.PrintWriter fwrite = new java.io.PrintWriter(filename); //create file out
+    java.io.Scanner fread = new java.io.Scanner(fd); //create file in
+    fread.useDelimeter(DELIM);
 
     //initialize array to hold data from file.
-    int[] randoms = 0;
+    int[] randoms;
     randoms = new int[QUANTITY];
 
+    //ensure file exists
+    if (!fd.isFile()) {
+      //file does not exist, create now.
+      AppendTxtFile(fwrite);
+    }
+
     //read in data from file, explode into int array randoms
+    int i = 0;
+    while (fread.hasNext() && i <= QUANT) {
+      randoms[i] = fread.nextInt();
+      i++;
+    }
 
     //sort the data that's been read in
-    SortInts();
+    SortInts(randoms);
 
     //print neat table of data in randoms array
   }
@@ -54,11 +66,26 @@ public class ReadFiles11 {
    * Generates [QUANT] random integers which are immediately passed into a file on
    * disk.
    *
-   * @param  object  File object being used
-   * @return int     status code of file creation. 0 upon success.
+   * @param  object  PrintWriter object being used
+   * @return int     status code of file composition.
+   *  - 0 upon success
+   *  - 1 upon failure.
    */
   public static int AppenTxtFile(java.io.File f) {
-    //generate [QUANTITY], [DELIM] seperated, ints into fname.txt
+    int i = 0;
+    while (i < 100) {
+      int r = (int)(Math.Random() * MAX);
+      try (e) {
+        f.printf("%d%c", r, DELIM);
+      }
+      catch {
+        f.close();
+        return 1;
+      }
+      i++;
+    }
+    f.close();
+
     return 0;
   }
 
