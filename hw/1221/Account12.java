5b6d6da23febdd934146fe7707a000cdaaa2f9ea
Jonathan Zacsh <jzacsh@gmail.com>
2010-11-09 08:50:27 -0500

    starting 1221

---
+++ hw/1221/Account12.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.*;
+
+/**
+ * - course:     COMP-171-801RL
+ * - assignment: pg 125; que. #4.21; due: 10/19
+ *
+ * @author Jonathan Zacsh <jzacsh@gmail.com>
+ */
+public class Account12 {
+  public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+
+    //printed documentation
+    String intro  = "Jonathan Zacsh <jzacsh@gmail.com>; COMP-171-801RL\n";
+           intro += "pg 256; que. #7.3; due: 12/21\n";
+    String summary = "\n";
+    System.out.printf("\n%s\n%s", intro, summary);
+
+    //codez
+  }
+}
+
