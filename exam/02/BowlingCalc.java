1ea702431e34c7256d88616f9df606f64b5d4b31
Jonathan Zacsh <jzacsh@gmail.com>
2010-11-23 22:07:21 -0500

    draft of pmethods

---
+++ exam/02/BowlingCalc.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ * Print each player's bowling average for a match and the team's average.
+ * - from ./readme
+ *   "The average is the sum of the game scores divided by the number of
+ *   games."
+ */
+public class BowlingCalc {
+  double MAXSCORE = 300;
+  double TEAMSIZE = 5;
+  double GAMEQTY  = 3;
+  public static void main(String[] args) {
+    String intro  = "I will calculate the averages for the bowling team and its"
+           intro .= "players.\n";
+    System.out.printf(intro);
+
+    //
+  }
+
+  public static double playerAverage(double[] scores) {
+    //
+  }
+
+  public static double teamAverage(double[][] scores) {
+    //
+  }
+}
