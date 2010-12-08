6795c14cbeffea78183e2885dfb0371d270ef72e
Jonathan Zacsh <jzacsh@gmail.com>
2010-12-08 00:40:46 -0500

    ton of doxygen for this useless assignmentt st.

--- hw/1221/Account12.java
+++ hw/1221/Account12.java
@@ -31,23 +31,47 @@ public class Account12 {
 
 
   //accessors:
-
+  /**
+   * Get the current account id
+   * 
+   * @return  int  account id.
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Get the current account balance.
+   * 
+   * @return  double  account balance.
+   */
   public double getBalance() {
     return balance;
   }
 
+  /**
+   * Get the current interest rate.
+   *
+   * @return  double  interest rate.
+   */
   public double getRate() {
     return annualInterestRate;
   }
 
+  /**
+   * Get the account's creation date.
+   *
+   * @return  Date  account creation date.
+   */
   public Date getCreationDate() {
     return dateCreated;
   }
 
+  /**
+   * Calculate the current monthly interest rate.
+   *
+   * @return  double  monthly interest rate, based on current Annual rate.
+   */
   public double getMonthlyInterestRate() {
     return annualInterestRate / 12;
   }
@@ -55,36 +79,70 @@ public class Account12 {
 
   //mutators:
 
+  /**
+   * Set the current account id to an arbitrary number.
+   *
+   * @param  int  new account id.
+   */
   public void setId(int acctId) {
     id = acctId;
   }
 
+  /**
+   * Set the current balance to an arbitrary amount.
+   *
+   * @param  double  new account balance.
+   */
   public void setBalance(double acctBal) {
     balance = acctBal;
   }
 
+  /**
+   * Set the current interest rate to an arbitrary amount.
+   *
+   * @param  double  new rate of interest
+   */
   public void setRate(double acctRate) {
     annualInterestRate = acctRate;
   }
 
   //basic methods:
 
+  /**
+   * Wrapper to deposit(), emulating a withdraw.
+   *
+   * @param   double  positive value representing amount withdrawn
+   * @return  double  current, resulting balance
+   */
   public double withDraw(double amt) {
-    balance -= amt;
-    return balance;
+    return deposit(-1 * amt);
   }
 
+  /**
+   * Increment value in balance property of current object.
+   *
+   * @param   double  positive value representing amount withdrawn
+   * @return  double  current, resulting balance
+   */
   public double deposit(double amt) {
     balance += amt;
     return balance;
   }
 
+  /**
+   * Main class's function, run when class is called directly. Serves as proof
+   * of concept run; instantiates an example object with test data.
+   *
+   * @param  String  ignored arguments.
+   */
   public static void main(String[] args) {
     //printed documentation
     String intro  = "Jonathan Zacsh <jzacsh@gmail.com>; COMP-171-801RL\n";
            intro += "pg 256; que. #7.3; due: 12/21  (lab# 12)\n";
     String summary = "This is a very basic class for managing a bank account.\n";
     System.out.printf("\n%s\n%s", intro, summary);
+
+    // proof of concept object for Account12 class.
   }
 }
 
